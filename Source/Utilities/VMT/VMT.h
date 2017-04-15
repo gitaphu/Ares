8d4bc1724e2639c4db2cbb806aa661b44e67ce20
aphu <aphu@420blaze.it>
2017-04-15 18:35:50 -0400

    Update VMT.h

--- Source/Utilities/VMT/VMT.h
+++ Source/Utilities/VMT/VMT.h
@@ -53,8 +53,30 @@ public:
     return reinterpret_cast<Fn>(this->m_piOriginalVFT[strFunctionIndex]);
   }
 
-  inline bool HookFunction(void* pNewFunction, const std::size_t strFunctionIndex);
-  inline bool UnhookFunction(const std::size_t strFunctionIndex);
-  inline std::size_t GetTotalFunctions();
+  inline bool CVMT::HookFunction(void* pNewFunction, const std::size_t strFunctionIndex) {
+
+    if (strFunctionIndex > this->m_strTotalFunctions) // Make sure you're not calling a function that doesn't exist. :P
+      return false;
+
+    this->m_piCurrentVFT[strFunctionIndex] = reinterpret_cast<std::uintptr_t>(pNewFunction); // Take the old pointer to the VFUNC and replace it with our own.
+
+    return true;
+
+  }
+
+  inline bool CVMT::UnhookFunction(const std::size_t strFunctionIndex) {
+
+    if (strFunctionIndex > this->m_strTotalFunctions) // Make sure you're not calling a function that doesn't exist.
+      return false;
+
+    this->m_piCurrentVFT[strFunctionIndex] = this->m_piOriginalVFT[strFunctionIndex];
+
+    return true;
+
+  }
+
+  inline std::size_t CVMT::GetTotalFunctions() {
+    return this->m_strTotalFunctions;
+  }
 
 };
